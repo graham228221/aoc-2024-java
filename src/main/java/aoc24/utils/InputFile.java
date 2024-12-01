61671fec2eb30fef216d5d6186a0f58cb1861d0d
Graham Hopgood <graham.hopgood@guardian.co.uk>
2024-12-01 11:49:30 +0000

    tidy inputfile helper

--- src/main/java/aoc24/utils/InputFile.java
+++ src/main/java/aoc24/utils/InputFile.java
@@ -8,20 +8,29 @@ import java.util.List;
 public class InputFile {
     
     public List<String> inputLines;
+    public List<String> testLines;
 
     public InputFile(String fileName) {
         inputLines = getLines(fileName);
+        testLines = getTestLines(fileName);
+    }
+
+    private static List<String> getTestLines(String fileName) {
+        try (BufferedReader br = new BufferedReader(
+            new FileReader("src/main/java/aoc24/resources/"+fileName+"_example.txt")
+        )) {
+            return br.lines().toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException(e);
+        }
     }
 
     private static List<String> getLines(String fileName) {
-        try {
-
-            BufferedReader br = new BufferedReader(
-                new FileReader("src/main/java/aoc24/resources/"+fileName+".txt")
-            );
-
+        try (BufferedReader br = new BufferedReader(
+            new FileReader("src/main/java/aoc24/resources/"+fileName+".txt")
+        )) {
             return br.lines().toList();
-
         } catch (IOException e) {
             e.printStackTrace();
             throw new IllegalStateException(e);
